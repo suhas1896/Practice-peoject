d56d2928f471dad5be692ac24320417692fee56c
Suhas <suhasmandya121@gmail.com>
2022-01-27 10:55:08 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Practice-peoject';
+  constructor(private http:HttpClient){}
+
+  ngOnInit(){
+    console.log("hello Suhas")
+    let url = ('https://jsonplaceholder.typicode.com/todos/1');
+    this.http.get(url).subscribe((data)=>{
+      console.log(data)
+    })
+  }
 }
